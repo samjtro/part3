4c0db33be2d7293cda514e7579c588f62ebdac97
samjtro <hi@samjtro.com>
2024-08-18 08:38:29 -0500

    finished -> ex3.21, attempting to fix errors for 3.22

--- models/entry.js
+++ models/entry.js
@@ -4,6 +4,37 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery',false)
 mongoose.connect(process.env.MONGODB_URI)
 
+const isANumber = ({n}) => {
+    if (n >= '0' && n <= '9') {
+        return true
+    } else {
+        return false
+    }
+}
+
+const validatePhoneNumber = (n) => {
+    var t = false
+    var arr = n.split("")
+    if (n.split("-").length > 2) {
+        return(t)
+    }
+    var count = 0
+    arr.forEach(i => {
+        if (!isANumber(i)) {
+            return(t)
+        } else if(i==="-") {
+            count = count + 1
+        }
+    })
+    if (arr.indexOf("-")===-1) {
+        return(t)
+    } else if (arr.indexOf("-")<2 || arr.indexOf("-")>3) {
+        return(t)
+    }
+    t = true
+    return(t)
+}
+
 const entrySchema = new mongoose.Schema({
     name: {
         type: String,
@@ -12,7 +43,9 @@ const entrySchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        required: true
+        minLength: 8,
+        required: true,
+        validate: validatePhoneNumber
     },
 })
 entrySchema.set('toJSON', {
